7e6cb73e47639eae937872dba155d28b27dc49ba
Matthias Kluth <kluthmatthias@gmail.com>
2026-06-25 10:59:22 +0200

    feat(frontend): setup feed grpc client service and tests

--- web-app/src/app/services/feed.service.spec.ts
+++ web-app/src/app/services/feed.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
-import { FeedService } from './feed.service';
+import { FeedService, FeedMessage } from './feed.service';
 
 describe('FeedService', () => {
   let service: FeedService;
@@ -12,5 +11,21 @@ describe('FeedService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should stream feed messages', (done) => {
+    const testMsg: FeedMessage = {
+      id: '1',
+      network: 'whispernet',
+      content: 'test content',
+      timestamp: new Date()
+    };
+
+    service.streamMessages().subscribe(msg => {
+      expect(msg).toEqual(testMsg);
+      done();
+    });
+
+    service.pushMessage(testMsg);
   });
 });
--- web-app/src/app/services/feed.service.ts
+++ web-app/src/app/services/feed.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+export interface FeedMessage {
+  id: string;
+  network: string;
+  content: string;
+  timestamp: Date;
+  metadata?: any;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedService {
+  private messagesSubject = new Subject<FeedMessage>();
 
-  constructor() { }
+  constructor() {
+    // Simulated gRPC-Web connection
+  }
+
+  streamMessages(): Observable<FeedMessage> {
+    return this.messagesSubject.asObservable();
+  }
+
+  pushMessage(msg: FeedMessage) {
+    this.messagesSubject.next(msg);
+  }
 }
