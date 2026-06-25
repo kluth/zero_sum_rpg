9ab1aae1e3d50bcb21e0b8b08c3b01465befe0c9
Matthias Kluth <kluthmatthias@gmail.com>
2026-06-25 11:01:25 +0200

    feat(frontend): create frequenz-x component logic

---
+++ web-app/src/app/ui/frequenz-x/frequenz-x.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FeedService, FeedMessage } from '../../services/feed.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-frequenz-x',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './frequenz-x.component.html',
+  styleUrls: ['./frequenz-x.component.css']
+})
+export class FrequenzXComponent implements OnInit, OnDestroy {
+  messages: FeedMessage[] = [];
+  isHijacked: boolean = true;
+  private sub?: Subscription;
+
+  constructor(private feedService: FeedService) {}
+
+  ngOnInit(): void {
+    this.sub = this.feedService.streamMessages().subscribe(msg => {
+      if (msg.network === 'frequenz-x') {
+        this.messages.push(msg);
+        if (this.messages.length > 30) this.messages.shift();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
+}
