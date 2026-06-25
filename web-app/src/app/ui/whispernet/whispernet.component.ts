654d8434af7250c8203d3008d822d5c246acc356
Matthias Kluth <kluthmatthias@gmail.com>
2026-06-25 10:59:01 +0200

    feat: incremental update for whispernet.component.ts

---
+++ web-app/src/app/ui/whispernet/whispernet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whispernet',
+  standalone: true,
+  imports: [],
+  templateUrl: './whispernet.component.html',
+  styleUrl: './whispernet.component.css'
+})
+export class WhispernetComponent {
+
+}
