aa1983470bd1930e0a93c25c7e7f75c554383930
Matthias Kluth <kluthmatthias@gmail.com>
2026-06-25 11:00:46 +0200

    feat(frontend): create ocgf component logic

---
+++ web-app/src/app/ui/ocgf/ocgf.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FeedService, FeedMessage } from '../../services/feed.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-ocgf',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ocgf.component.html',
+  styleUrls: ['./ocgf.component.css']
+})
+export class OcgfComponent implements OnInit, OnDestroy {
+  feedItems: FeedMessage[] = [];
+  citizenId: string = 'CTZ-492-811';
+  showAlert: boolean = false;
+  alertMessage: string = '';
+  private sub?: Subscription;
+
+  constructor(private feedService: FeedService) {}
+
+  ngOnInit(): void {
+    this.sub = this.feedService.streamMessages().subscribe(msg => {
+      if (msg.network === 'ocgf') {
+        if (msg.metadata?.isAlert) {
+          this.triggerAlert(msg.content);
+        } else {
+          this.feedItems.unshift(msg);
+          if (this.feedItems.length > 20) this.feedItems.pop();
+        }
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
+
+  private triggerAlert(message: string) {
+    this.alertMessage = message;
+    this.showAlert = true;
+    setTimeout(() => {
+      this.showAlert = false;
+    }, 5000);
+  }
+}
