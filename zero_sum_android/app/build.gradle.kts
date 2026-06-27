619df5022dbcd1bfa7ec57650a7b9c93db2ca0e2
Matthias Kluth <kluthmatthias@gmail.com>
2026-06-27 10:58:03 +0200

    chore(android): bump targetSdk and compileSdk to 35 to meet Play Console 2026 requirements

--- zero_sum_android/app/build.gradle.kts
+++ zero_sum_android/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.zerosum.rpg"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "cloud.kluth.zero_sum"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 2
         versionName = "1.0.1"
 
